cafe9e6d8c9e3dc0a3ca74fc0c29b37957720c31
Darshil Thummar <thummardarshil9687@gmail.com>
2024-05-11 18:23:20 +0530

    api problem fixed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -13,28 +13,28 @@ const Feed = () => {
 
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
       .then((data) => setVideos(data.items))
-    }, [selectedCategory]);
+  }, [selectedCategory]);
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-        
+
         <Typography className="copyright" variant="body2" sx={{
-  mt: 1.5,
-  color: "#fff",
-  "& a": {
-    color: "#fff",
-    textDecoration: "none",
-    borderBottom: "1px solid #fff",
-    transition: "border-bottom 0.2s ease",
-    "&:hover": {
-      borderBottom: "1px solid transparent",
-    },
-  },
-}}>
-  © 2023 ❤️ <a href="https://darshil25.github.io/">Darshil Thummar</a>
-</Typography>
+          mt: 1.5,
+          color: "#fff",
+          "& a": {
+            color: "#fff",
+            textDecoration: "none",
+            borderBottom: "1px solid #fff",
+            transition: "border-bottom 0.2s ease",
+            "&:hover": {
+              borderBottom: "1px solid transparent",
+            },
+          },
+        }}>
+          © 2023 ❤️ <a href="https://darshil25.github.io/">Darshil Thummar</a>
+        </Typography>
 
       </Box>
 
